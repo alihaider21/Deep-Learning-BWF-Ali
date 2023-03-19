3d87ddc027047822094d4f9b7b6ea0393af7a11b
Ali Haider <alidevolper@gmail.com>
2023-03-19 11:00:55 +0500

    Task 3 & 4 added

---
+++ Task- 3 & 4/simple_message.py
@@ -0,0 +1,13 @@
+#2-1. Simple Message
+message = "Hello Bytewise Limited :)"
+print(message)
+
+#2-2. Simple Messages
+message_1 = "Hi There"
+print(message_1)
+
+message_1 = "Hi There here is the new message"
+print(message_1)
+
+
+
