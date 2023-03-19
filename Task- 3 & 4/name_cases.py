7ed68ecf2dfcd0c85bfdf89ca639c2f4d555d672
Ali Haider <alidevolper@gmail.com>
2023-03-19 18:30:28 +0500

    python list file added

---
+++ Task- 3 & 4/name_cases.py
@@ -0,0 +1,37 @@
+#2-3. Personal Message
+name = "Ali Haider"
+print(f"Hello {name} would you like to learn some Python today?")
+
+#2-4. Name Cases
+name = "Ali Haider"
+print(name.upper()) #uppercase
+print(name.lower()) #lowercase
+print(name.title()) #titlecase
+
+#2-5. Famous Quote
+print('Albert Einstein once said, "It takes a touch of genius \n and a lot of courage o move in the opposite direction.." ')
+
+#2-6. Famous Quote 2
+famous_person = "Linus Torvalds"
+message = 'Talk is cheap. \n Show me the code'
+print(famous_person," once said, ",message)
+
+#2-7. Stripping Names: 
+name = "  Ali\nHaider "
+print(name)
+print(name.strip().lstrip().rstrip())
+
+#2-8. Number Eight
+print(5 + 3)
+print(16 - 8)
+print(4 * 2)
+print(16 // 2)
+
+#2-9. Favorite Number: 
+
+x = 12
+msg = "Hey!!! Here is your fav number: "
+print(msg,x)
+
+
+
---
+++ Task- 3 & 4/python_list.py
@@ -0,0 +1,15 @@
+#3-1. Names:
+Names = ['Ali','Usman','Shazam','Zahid']
+for i in range(0,len(Names)):
+    print(Names[i])
+
+
+#3-2. Greetings
+for i in range(0,len(Names)):
+    print("Hello!",Names[i])
+
+#3-3. Your Own List
+transport = ['Honda CG150cc','Yamaha R6', 'Suzuki GSXR 1000','Suzuki Hayabusa']
+for i in range(0,len(transport)):
+    print("I would like to own a",transport[i])
+
