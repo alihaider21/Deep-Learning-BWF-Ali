5c1f28c989740ede92db3fc9052e557d879c734a
Ali Haider <alidevolper@gmail.com>
2023-03-21 21:16:17 +0500

    task 5 file added

--- Task-5/timing_code.py
+++ Task-5/timing_code.py
@@ -1,5 +1,5 @@
 #There are several ways to check the execution time of your code 
-#In this exercise i will use 2 diffrent ways to check the execution time of your code
+#In this exercise i will use time.time to check the execution time of your code
 
 #Example 1: Using time.time()
 import time
@@ -7,19 +7,36 @@ import time
 start_time = time.time()
 
 # Your code here
+set1 = {1, 2, 3}
+set2 = {3, 4, 5}
+union_set = set1.union(set2)
+print(union_set)
+
 
 end_time = time.time()
 
 print(f"Execution time: {end_time - start_time} seconds")
 
 
-#Example 2: Using timeit
-import timeit
+#Example 2: Using time.time()
+import time
+start_time = time.time()
 
-code_to_test = """
-# Your code here
-"""
+age = 35
 
-execution_time = timeit.timeit(code_to_test, number=1)
+if age < 2:
+    print("This person is a baby.")
+elif age < 4:
+    print("This person is a toddler.")
+elif age < 13:
+    print("This person is a kid.")
+elif age < 20:
+    print("This person is a teenager.")
+elif age < 65:
+    print("This person is an adult.")
+else:
+    print("This person is an elder.")
 
-print(f"Execution time: {execution_time} seconds")
+end_time = time.time()
+
+print(f"Execution time: {end_time - start_time} seconds")
